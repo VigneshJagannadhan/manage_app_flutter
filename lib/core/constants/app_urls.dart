c32b5584b335f181f9573b6cc9781d7f2686b08b
VigneshKJagannadhan <vigneshjagannadhan1998@gmail.com>
2026-08-21 13:42:00 +0530

    chore: remove unused dev base url constant

--- lib/core/constants/app_urls.dart
+++ lib/core/constants/app_urls.dart
@@ -1,6 +1,5 @@
 class AppUrls {
   static const String baseUrl = 'https://manage-app-api.onrender.com/api';
-  static const String devBaseUrl = String.fromEnvironment('API_BASE_URL', defaultValue: 'http://localhost:3000/api');
   static const String signUp = '/auth/signup';
   static const String signIn = '/auth/signin';
   static const String refreshToken = '/auth/refresh';
